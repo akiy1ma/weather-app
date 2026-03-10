c13d482540e98bb0a5636b3690d666e1702146a5
akiyama <iwanttob2.7lieve@gmail.com>
2026-03-10 15:46:07 +0300

    css adjustments for future features

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+import "../src/style.css"
 import { API_KEY } from "../API"
 
+const currentLocation = document.querySelector(".current-location")
+const searchBar = document.querySelector(".searchbar");
+searchBar.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        getWeather(searchBar.value);
+    }
+})
+
 const baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
-let location = "london";
+//let location = "london";
 const unitGroup = {
     us: "?unitGroup=us",
     metric: "?unitGroup=metric"
 }
 
-async function getWeather() {
+async function getWeather(location) {
     const responce = await fetch(baseURL + location + unitGroup.metric + "&key=" + API_KEY +
         "&include=current&elements=temp,feelslike,humidity,icon,precip,precipprob,snow,winddir,windspeed"
     );
